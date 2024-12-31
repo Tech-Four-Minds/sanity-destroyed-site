934f0bc787d8e5986ba7b4df9481d2c4db24aec4
LucasRamuus <lucasgabrielpr12@gmail.com>
2024-12-31 18:12:46 -0300

    feat: create usecase-event

---
+++ api/src/domain/usecases/event/get-event-by-id.usecase.ts
@@ -0,0 +1,34 @@
+import { EventProps } from "../../entity/event";
+import { EventGateway } from "../../gateway/event.gateway";
+import { BaseUsecase } from "../usecase";
+
+export class getEventByIdUseCase extends BaseUsecase<string, EventProps | null> {
+    constructor(private eventGateway: EventGateway) {
+        super();
+    }
+
+    async execute(id: string): Promise<EventProps | null> {
+        this.validateInput(id);
+
+        try {
+
+            const event = await this.eventGateway.getEventById(id);
+
+            this.logAction(`Evento ${id} buscado com sucesso`);
+
+            return event;
+            
+        } catch (error: unknown) {
+            
+            if (error instanceof Error) {
+                this.handleError(error);
+
+            } else {
+                this.handleError(new Error("Erro ao buscar o evento"))
+            }
+
+            return null;
+        }
+        
+    }
+}
---
+++ api/src/domain/usecases/usecase.ts
@@ -0,0 +1,28 @@
+export interface Usecase<InputDto, OutputDto>{
+    execute(input: InputDto): Promise<OutputDto>;
+    validateInput(input: InputDto): void;
+    handleError(error: Error): void;
+    logAction(action: string): void;
+}
+
+export abstract class BaseUsecase<InputDto, OutputDto> implements Usecase<InputDto, OutputDto> {
+    abstract execute(input: InputDto): Promise<OutputDto>;
+
+    validateInput(input: InputDto): void {
+        if(!input) {
+            throw new Error("Entrada inválida");
+        }
+    }
+
+    handleError(error: Error): void {
+        console.error("Erro ao processar a operação:", error.message);
+        throw new Error("Operação falhou. Tente novamente mais tarde.");
+
+    }
+
+    logAction(action: string): void {
+        
+        console.log(`Ação realizada: ${action}`);
+    }
+
+}
